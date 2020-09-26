a1972d096addb9d93452e66354b696e97f5365b7
B Joe <bjoe322@aucklanduni.ac.nz>
2020-09-26 17:48:05 +1200

    Side-track task: helping to implement PracticeCategoryController

---
+++ src/quinzical/PracticeCategoryController.java
@@ -0,0 +1,11 @@
+package quinzical;
+
+import javafx.event.ActionEvent;
+
+import java.io.IOException;
+
+public class PracticeCategoryController {
+    public void onStartClick(ActionEvent event) throws IOException {
+        new SceneSwitcher().switchScene(event, "PracticeAnswer.fxml");
+    }
+}
--- src/quinzical/model/Question.java
+++ src/quinzical/model/Question.java
@@ -21,9 +21,9 @@ public class Question {
         // Replacing Maori long vowels with double letter equivalents (may change)
         this.answers = macronSubstitute(answer.strip().toLowerCase())
                 .split("/");
-        System.out.println("CLUE: " + this.clue);
-        System.out.println("PREFIX: " + this.getPrefix());
-        System.out.println("ANSWERS: " + this.getAnswers());
+        //System.out.println("CLUE: " + this.clue);
+        //System.out.println("PREFIX: " + this.getPrefix());
+        //System.out.println("ANSWERS: " + this.getAnswers());
     }
     public String macronSubstitute(String oString) {
         return oString.replace("ā", "aa")
@@ -52,7 +52,7 @@ public class Question {
         return this.answerPrefix.substring(0,1).toUpperCase() + this.answerPrefix.substring(1);
     }
     public String getAnswers() {
-        return Arrays.toString(answers).replaceAll("\\[|\\]", "");
+        return Arrays.toString(answers).replaceAll("[\\[\\]]", "");
     }
     public boolean isAnswered() {
         return this.answered;
