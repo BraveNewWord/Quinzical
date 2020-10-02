787aca8475d36e12eb223b38aad91daec58bad48
jyao244 <68982751+jyao244@users.noreply.github.com>
2020-10-02 18:06:18 +1300

    Update PracticeCategoryController.java
    fixed exception

--- src/quinzical/PracticeCategoryController.java
+++ src/quinzical/PracticeCategoryController.java
@@ -6,17 +6,20 @@ import java.util.List;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import quinzical.model.Category;
 import quinzical.model.PracticeManager;
 
 public class PracticeCategoryController {
 	@FXML private ComboBox<String> cb;
+	@FXML private Label errorLabel;
 	private PracticeManager pm;
 	private List<Category> categories=new ArrayList<Category>();
 	private List<String> categoriesString=new ArrayList<String>();
 	private StringSpeaker stringSpeaker;
 	
 	public void initialize(PracticeManager pm, StringSpeaker stringSpeaker) throws Exception {
+		errorLabel.setText("");
 		this.stringSpeaker = stringSpeaker;
 		this.pm=pm;
 		this.pm.setCategories();
@@ -28,15 +31,19 @@ public class PracticeCategoryController {
 	}
 	
 	public void onStartClick(ActionEvent event) throws Exception {
-		String categoryString=cb.getValue();
-		for(Category c:categories) {
-			if(categoryString.equals(c.getName())) {
-				this.pm.chooseCategory(c);
+		try {
+			String categoryString=cb.getValue();
+			for(Category c:categories) {
+				if(categoryString.equals(c.getName())) {
+					this.pm.chooseCategory(c);
+				}
 			}
+	        PracticeAnswerController controller = new SceneSwitcher().
+	                switchScene(event, "PracticeAnswer.fxml").getController();
+	        controller.initialize(this.pm, this.stringSpeaker);
+		}catch (Exception e){
+			errorLabel.setText("Please select a category");
 		}
-        PracticeAnswerController controller = new SceneSwitcher().
-                switchScene(event, "PracticeAnswer.fxml").getController();
-        controller.initialize(this.pm, this.stringSpeaker);
     }
 	
 	public void onExitClick(ActionEvent event) throws Exception {
